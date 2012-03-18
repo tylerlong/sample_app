ca6d994484e040ebd5ad408b12a6c34c038b90c5
Tyler Long <tyler4long@gmail.com>
2012-03-18 01:52:28 -0700

    add the contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Sample App" }
+
   describe "Home page" do
   	it "should have the h1 'Home'" do
   		visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe "Static pages" do
   	end
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	page.should have_selector('title', :text => "Sample App | Home")
+    	page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -20,7 +22,7 @@ describe "Static pages" do
   	end
   	it "should have the title 'Help'" do
   		visit '/static_pages/help'
-  		page.should have_selector('title', :text => "Sample App | Help")
+  		page.should have_selector('title', :text => "#{base_title} | Help")
   	end
   end
 
@@ -31,8 +33,19 @@ describe "Static pages" do
   	end
   	it "should have the title 'About'" do
   		visit '/static_pages/about'
-  		page.should have_selector('title', :text => "Sample App | About")
+  		page.should have_selector('title', :text => "#{base_title} | About")
   	end
+  end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
   end
 
 end
