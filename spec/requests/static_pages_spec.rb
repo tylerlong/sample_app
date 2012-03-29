b4d27e09968fef45754b5dfa64cd8f1ef714008f
Tyler Long <tyler4long@gmail.com>
2012-03-29 07:38:53 -0700

    fix typo

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,7 +18,7 @@ describe "Static pages" do
     page.should have_selector('title', text: full_title('Sign up'))
   end  
 
-  share_examples_for "all static pages" do
+  shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }
     it { should have_selector('title', text: full_title(page_title)) }
   end
