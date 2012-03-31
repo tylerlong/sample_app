b12def6e86bca5cb50c1372be3b0dfa46c8f9481
Tyler Long <tyler4long@gmail.com>
2012-03-31 08:06:05 -0700

    add user sign in form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+    else
+      # Create an error message and re-render the signin form.
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
