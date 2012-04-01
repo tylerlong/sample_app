86ef02bdc805b6a7e4b6b38ab759ec6e02a8ef3a
Tyler Long <tyler4long@gmail.com>
2012-04-01 09:35:29 -0700

    Require user to login before editing profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :sign_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -33,5 +34,10 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private
+    def sign_in_user
+      redirect_to signin_path, notice: "Please sign in" unless signed_in?
+    end
 
 end
