28a466f874bb0310a50ae3d7c42da8d5eef9d5f5
Tyler Long <tyler4long@gmail.com>
2012-04-01 10:33:50 -0700

    Require the right user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
 
   def sign_in(user)
-    session[:remember_token] = user.remember_token
+    cookies[:remember_token] = user.remember_token
     current_user = user
   end
 
@@ -11,7 +11,7 @@ module SessionsHelper
 
   def sign_out
     current_user = nil
-    session.delete(:remember_token)
+    cookies.delete(:remember_token)
   end
 
   def current_user=(user)
@@ -22,10 +22,14 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   private
 
     def user_from_remember_token
-      remember_token = session[:remember_token]
+      remember_token = cookies[:remember_token]
       User.find_by_remember_token(remember_token) unless remember_token.nil?
     end
 
