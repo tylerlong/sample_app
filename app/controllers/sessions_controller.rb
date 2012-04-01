0260507fc0716b9f3de520ce2ff2fbbf8ef49e06
Tyler Long <tyler4long@gmail.com>
2012-03-31 22:12:19 -0700

    display sign in failure messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page.
     else
-      # Create an error message and re-render the signin form.
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
