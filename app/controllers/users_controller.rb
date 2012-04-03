a6f446976fa69bc8bd5b7945d46d537556cb213a
Tyler Long <tyler4long@gmail.com>
2012-04-03 07:28:56 -0700

    Add user list page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :sign_in_user, only: [:edit, :update]
+  before_filter :sign_in_user, only: [:edit, :update, :index]
   before_filter :correct_user, only: [:edit, :update]
 
   def show
@@ -32,6 +32,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   private
