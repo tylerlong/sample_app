abec1653147036970881d24ef96132bfc0b271b8
Tyler Long <tyler4long@gmail.com>
2012-04-23 02:03:09 -0700

    Able to follow others and see feeds

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -53,6 +53,16 @@ describe "Static pages" do
           it { should have_content("1 micropost") }
         end
       end
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 follower",  href: followers_user_path(user)) }
+      end
     end
   end
 
